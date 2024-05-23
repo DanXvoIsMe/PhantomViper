0622045020b94b1d65a196586156359514ae5f0e
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2024-05-23 11:14:30 +0200

    Create MainScript.lua

---
+++ MainScript.lua
@@ -0,0 +1,403 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Consistt/Ui/main/UnLeaked"))()
+local PLAYER = game.Players.LocalPlayer
+local CurrentCam  = game.Workspace.CurrentCamera
+local UIS = game:GetService("UserInputService")-- Made By Mick Gordon
+local WorldToViewportPoint = CurrentCam.WorldToViewportPoint
+local mouseLocation = UIS.GetMouseLocation
+
+local Fov = Instance.new("ScreenGui")Fov.Name = "Fov" Fov.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui") Fov.ZIndexBehavior = Enum.ZIndexBehavior.Sibling Fov.ResetOnSpawn = false-- i miss you synapse fov
+local TracersG = Instance.new("ScreenGui")TracersG.Name = "Tracers" TracersG.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui") TracersG.ZIndexBehavior = Enum.ZIndexBehavior.Sibling TracersG.ResetOnSpawn = false
+local FOVFFrame = Instance.new("Frame")FOVFFrame.Parent = Fov FOVFFrame.Name = "FOVFFrame" FOVFFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255) FOVFFrame.BorderColor3 = Color3.fromRGB(0, 0, 0) FOVFFrame.BorderSizePixel = 0 FOVFFrame.BackgroundTransparency = 1 FOVFFrame.AnchorPoint = Vector2.new(0.5, 0.5) FOVFFrame.Position = UDim2.new(0.5, 0,0.5, 0) FOVFFrame.Size = UDim2.new(0, 70000, 0, 70000) FOVFFrame.BackgroundTransparency = 1 
+local UICorner = Instance.new("UICorner")UICorner.CornerRadius = UDim.new(1, 0) UICorner.Parent = FOVFFrame -- Made By Mick Gordon
+local UIStroke = Instance.new("UIStroke")UIStroke.Color = Color3.fromRGB(100,0,100) UIStroke.Parent = FOVFFrame UIStroke.Thickness = 1 UIStroke.ApplyStrokeMode = "Border" game:GetService("StarterGui"):SetCore("SendNotification", {Title = "https://discord.gg/FsApQ7YNTq", Text = "The Discord For More!"})
+local BoxC = Instance.new("ScreenGui", game.Workspace) BoxC.Name = "Box"
+local Higlight = Instance.new("ScreenGui",game.Workspace)
+
+
+BOXES = false
+TRACERS = false
+AIMBOTXD = false
+
+function isVisible(p, ...)
+	return #CurrentCam:GetPartsObscuringTarget({ p }, { CurrentCam, PLAYER.Character, ... }) == 0 
+end
+
+function CameraGetClosestToMouse(Fov)
+	local AimFov = Fov
+	local targetPos = nil
+
+	for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+		if v ~= PLAYER then
+			if true then
+				if v.Character and v.Character:FindFirstChild("Head") and v.Character.Humanoid and v.Character.Humanoid.Health > 0 and not (v.Team == PLAYER.Team) then
+					local screen_pos, on_screen = WorldToViewportPoint(CurrentCam, v.Character["Head"].Position)
+					local screen_pos_2D = Vector2.new(screen_pos.X, screen_pos.Y)
+					local new_magnitude = (screen_pos_2D - mouseLocation(UIS)).Magnitude
+					if on_screen and new_magnitude < AimFov and isVisible(v.Character["Head"].Position, v.Character.Head.Parent) then
+						AimFov = new_magnitude
+						targetPos = v
+					end
+				end
+            end
+		end
+	end
+	return targetPos
+end
+
+
+local function aimAt(pos, smooth)
+	local AimPart = pos.Character:FindFirstChild("Head")
+	if AimPart then
+		local LookAt = nil
+		local Distance = math.floor(0.5+(PLAYER.Character:FindFirstChild"HumanoidRootPart".Position - pos.Character:FindFirstChild"HumanoidRootPart".Position).magnitude)
+		if Distance > 100  then
+			local distChangeBig = Distance / 10
+			LookAt = CurrentCam.CFrame:PointToWorldSpace(Vector3.new(0,0,-smooth * distChangeBig)):Lerp(AimPart.Position,.01) -- No one esle do camera smoothing ? tf
+		else-- Made By Mick Gordon
+			local distChangeSmall = Distance / 10
+			LookAt = CurrentCam.CFrame:PointToWorldSpace(Vector3.new(0,0,-smooth * distChangeSmall)):Lerp(AimPart.Position,.01) -- No one esle do camera smoothing ? tf
+		end
+		CurrentCam.CFrame = CFrame.lookAt(CurrentCam.CFrame.Position, LookAt)
+	end
+end
+
+
+local function AddTracers(Player) -- Tracers Without Lib OMG !!!!, Needs Some Adjustments To The End Pos
+	local tracer = Instance.new("Frame")
+	tracer.Parent = TracersG
+	tracer.Name = Player.Name
+	tracer.Active = false
+	tracer.AnchorPoint = Vector2.new(.5, .5)
+	tracer.Visible = false
+
+	local co = coroutine.create(function()
+		game:GetService("RunService").RenderStepped:Connect(function()
+			if Player ~= PLAYER and Player and Player.Character and Player.Character.FindFirstChild(Player.Character, "Humanoid") and Player.Character.Humanoid.Health > 0 then
+				local TargetPart = Player.Character:FindFirstChild("HumanoidRootPart")
+				local ScreenPoint, OnScreen = CurrentCam:WorldToScreenPoint(TargetPart.Position)
+				local distance 
+				-- Made By Mick Gordon
+				distance = math.floor(0.5+(game.Workspace.CurrentCamera.CFrame.Position - Player.Character:WaitForChild("HumanoidRootPart").Position).magnitude)
+
+				local screenpointmain = Vector2.new(ScreenPoint.X, ScreenPoint.Y + (2500 / distance)) --  / distance so it can be at the bottom of the box.
+				local posd = UIS:GetMouseLocation()
+				local MouseOrigin = Vector2.new(posd.X, posd.Y - 36)
+				local Origin = Vector2.new(CurrentCam.ViewportSize.X/2, CurrentCam.ViewportSize.Y - 1)
+				local Position = (Origin + screenpointmain) / 2
+				local Length = (Origin - screenpointmain).Magnitude
+				tracer.Rotation = math.deg(math.atan2(screenpointmain.Y - Origin.Y, screenpointmain.X - Origin.X))
+
+
+				Position = (Origin + screenpointmain) / 2
+				Length = (Origin - screenpointmain).Magnitude
+				tracer.Rotation = math.deg(math.atan2(screenpointmain.Y - Origin.Y, screenpointmain.X - Origin.X))
+
+
+				if OnScreen then
+					if TRACERS and OnScreen then
+						if true then
+							if Player.TeamColor == PLAYER.TeamColor then
+								tracer.Visible = false
+							else
+								tracer.Visible = true
+							end
+						else -- Made By Mick Gordon
+							tracer.Visible = true
+						end
+					else
+						tracer.Visible = false
+					end-- Made By Mick Gordon
+
+					if true then
+						tracer.BackgroundColor3 = Player.TeamColor.Color
+					end
+
+					tracer.BorderColor3 = Color3.fromRGB(27, 42, 53)
+					tracer.Position = UDim2.new(0, Position.X, 0, Position.Y)
+					tracer.Size = UDim2.new(0, Length, 0, 2)
+				else
+					tracer.Visible = false
+				end
+
+				if not (game:GetService"Players":FindFirstChild(Player.Name)) then
+					Fov:FindFirstChild(Player.Name):Destroy()
+					coroutine.yield()
+				end
+			else
+				tracer.Visible = false
+			end
+		end)
+	end)
+	coroutine.resume(co)
+end
+
+
+local function AddBox(player)
+	local bbg = Instance.new("BillboardGui", BoxC)
+	bbg.Name = player.Name
+	bbg.AlwaysOnTop = true
+	bbg.Size = UDim2.new(4,0,5.4,0)
+	bbg.ClipsDescendants = false
+	bbg.Enabled = false
+
+	local outlines = Instance.new("Frame", bbg)
+	outlines.Size = UDim2.new(1,0,1,0)
+	outlines.BorderSizePixel = 1
+	outlines.BackgroundTransparency = 1
+	local left = Instance.new("Frame", outlines)
+	left.BorderSizePixel = 1
+	left.Size = UDim2.new(0,(1),1,0)
+	local right = left:Clone()
+	right.Parent = outlines
+	right.Size = UDim2.new(0,-(1),1,0)   
+	right.Position = UDim2.new(1,0,0,0)
+	local up = left:Clone()-- Made By Mick Gordon
+	up.Parent = outlines
+	up.Size = UDim2.new(1,0,0,(1))
+	local down = left:Clone()
+	down.Parent = outlines
+	down.Size = UDim2.new(1,0,0,-(1))
+	down.Position = UDim2.new(0,0,1,0)
+
+	local info = Instance.new("BillboardGui", bbg)
+	info.Name = "info"
+	info.Size = UDim2.new(3,0,0,54)
+	info.StudsOffset = Vector3.new(3.6,-3,0)
+	info.AlwaysOnTop = true
+	info.ClipsDescendants = false
+	info.Enabled = false
+	local namelabel = Instance.new("TextLabel", info)
+	namelabel.Name = "namelabel"
+	namelabel.BackgroundTransparency = 1
+	namelabel.TextStrokeTransparency = 0
+	namelabel.TextXAlignment = Enum.TextXAlignment.Left
+	namelabel.Size = UDim2.new(0,100,0,18)
+	namelabel.Position = UDim2.new(0,0,0,0)
+	namelabel.Text = player.Name
+	local distancel = Instance.new("TextLabel", info)
+	distancel.Name = "distancelabel"
+	distancel.BackgroundTransparency = 1-- Made By Mick Gordon
+	distancel.TextStrokeTransparency = 0
+	distancel.TextXAlignment = Enum.TextXAlignment.Left
+	distancel.Size = UDim2.new(0,100,0,18)
+	distancel.Position = UDim2.new(0,0,0,18)
+	local healthl = Instance.new("TextLabel", info)
+	healthl.Name = "healthlabel"
+	healthl.BackgroundTransparency = 1
+	healthl.TextStrokeTransparency = 0
+	healthl.TextXAlignment = Enum.TextXAlignment.Left
+	healthl.Size = UDim2.new(0,100,0,18)
+	healthl.Position = UDim2.new(0,0,0,36)
+
+	local uill = Instance.new("UIListLayout", info)
+
+	local forhealth = Instance.new("BillboardGui", bbg)
+	forhealth.Name = "forhealth"
+	forhealth.Size = UDim2.new(4.5,0,6,0)
+	forhealth.AlwaysOnTop = true
+	forhealth.ClipsDescendants = false
+	forhealth.Enabled = false
+
+	local healthbar = Instance.new("Frame", forhealth)
+	healthbar.Name = "healthbar"
+	healthbar.BackgroundColor3 = Color3.fromRGB(40,40,40)
+	healthbar.BorderColor3 = Color3.fromRGB(0,0,0)
+	healthbar.Size = UDim2.new(0.04,0,0.9,0)
+	healthbar.Position = UDim2.new(0,0,0.05,0)
+	local bar = Instance.new("Frame", healthbar)
+	bar.Name = "bar"
+	bar.BorderSizePixel = 0
+	bar.BackgroundColor3 = Color3.fromRGB(94,255,69)
+	bar.AnchorPoint = Vector2.new(0,1)
+	bar.Position = UDim2.new(0,0,1,0)
+	bar.Size = UDim2.new(1,0,1,0)
+
+	-- Made By Mick Gordon
+	local co = coroutine.create(function()
+		while wait(0.1) do
+			if player ~= PLAYER and player and player.Character and player.Character.FindFirstChild(player.Character, "Humanoid") and player.Character.Humanoid.Health > 0 then
+				bbg.Adornee = player.Character.HumanoidRootPart
+				info.Adornee = player.Character.HumanoidRootPart
+				forhealth.Adornee = player.Character.HumanoidRootPart
+
+				if BOXES then
+					outlines.Visible = true
+				else
+					outlines.Visible = false
+				end
+
+				outlines.BackgroundTransparency = 1
+				
+				if player.Character:FindFirstChild("Humanoid") ~= nil then
+					healthl.Text = "Health: "..math.floor(player.Character:FindFirstChild"Humanoid".Health)
+					healthbar.bar.Size = UDim2.new(1,0,player.Character:FindFirstChild"Humanoid".Health/player.Character:FindFirstChild"Humanoid".MaxHealth,0)
+				end
+				healthl.Visible = false
+				healthbar.Visible = true
+
+
+				if BOXES then
+					namelabel.Visible = true
+				else
+					namelabel.Visible = false
+				end
+
+				-- Made By Mick Gordon
+				if BOXES then
+					distancel.Visible = true
+					if PLAYER.Character and PLAYER.Character:FindFirstChild("HumanoidRootPart") ~= nil then
+						distancel.Text = "Distance: "..math.floor(0.5+(PLAYER.Character:FindFirstChild"HumanoidRootPart".Position - player.Character:FindFirstChild"HumanoidRootPart".Position).magnitude)
+					end
+				else
+					distancel.Visible = false
+				end
+
+
+				if BOXES and player.TeamColor == PLAYER.TeamColor then
+					bbg.Enabled = false
+					info.Enabled = false
+					forhealth.Enabled = false
+				else
+					bbg.Enabled = true
+					info.Enabled = true
+					forhealth.Enabled = true
+				end
+				-- Made By Mick Gordon
+
+				if BOXES then
+					left.BackgroundColor3 = player.TeamColor.Color
+					right.BackgroundColor3 = player.TeamColor.Color
+					up.BackgroundColor3 = player.TeamColor.Color
+					down.BackgroundColor3 = player.TeamColor.Color
+					outlines.BackgroundColor3 = Color3.fromRGB(75,0,10)
+					outlines.BackgroundColor3 = Color3.fromRGB(75,0,10)
+					left.BackgroundColor3 = Color3.fromRGB(75,0,10)
+					right.BackgroundColor3 = Color3.fromRGB(75,0,10)
+					up.BackgroundColor3 = Color3.fromRGB(75,0,10)
+					down.BackgroundColor3 = Color3.fromRGB(75,0,10)
+					healthl.TextColor3 = Color3.fromRGB(75,0,10)
+					distancel.TextColor3 = Color3.fromRGB(75,0,10)
+					namelabel.TextColor3 = Color3.fromRGB(75,0,10)
+				end
+
+				if not (game:GetService"Players":FindFirstChild(player.Name)) then
+					BoxC:FindFirstChild(player.Name):Destroy()
+					coroutine.yield()
+				end-- Made By Mick Gordon
+			else
+				bbg.Enabled = false
+				bbg.Adornee = nil
+				info.Adornee = nil
+				info.Enabled = false
+				forhealth.Adornee = nil
+				forhealth.Enabled = false
+			end
+		end 
+	end)
+	coroutine.resume(co)
+end
+
+
+local Wm = library:Watermark("PhantomViper | 0.1V | " .. library:GetUsername())
+local FpsWm = Wm:AddWatermark("fps: " .. library.fps)
+coroutine.wrap(function()
+    while wait(.75) do
+        FpsWm:Text("fps: " .. library.fps)
+    end
+end)()
+
+
+local Notif = library:InitNotifications()
+
+local LoadingXSX = Notif:Notify("PhantomViper Loading...", 3, "information") -- notification, alert, error, success, information
+
+library.title = "PhantomViper"
+
+library:Introduction()
+wait(1)
+local Init = library:Init()
+
+local Tab1 = Init:NewTab("ESP")
+
+local Section1 = Tab1:NewSection("Box")
+local Label1 = Tab1:NewLabel("Box - Just esp", "left")
+
+local Toggle1 = Tab1:NewToggle("Boxes", false, function(value)
+    local vers = value and "on" or "off"
+    BOXES = value
+end)
+
+local Section2 = Tab1:NewSection("Tracers")
+local Label2 = Tab1:NewLabel("Tracers - Lines to ppls", "left")
+
+local Toggle2 = Tab1:NewToggle("Tracers", false, function(value)
+    local vers = value and "on" or "off"
+    TRACERS = value
+end)
+
+local Tab2 = Init:NewTab("Aim")
+local Section3 = Tab2:NewSection("Aimbot V1")
+local Label3 = Tab2:NewLabel("Aimbot - Automaticly aims to target head", "left")
+local Toggle3 = Tab2:NewToggle("Aimbot", false, function(value)
+    local vers = value and "on" or "off"
+    AIMBOTXD = value
+end)
+
+local Tab3 = Init:NewTab("TP")
+local Section4 = Tab3:NewSection("Teleport to enemy team")
+local Button1 = Tab3:NewButton("Random", function()
+    local players = game:GetService("Players"):GetPlayers()
+
+    local differentTeamPlayers = {}
+    for _, player in ipairs(players) do
+        if player ~= PLAYER and player.Team ~= PLAYER.Team then
+            table.insert(differentTeamPlayers, player)
+        end
+    end
+    local pickedPlayer = differentTeamPlayers[math.random(1, #differentTeamPlayers)]
+
+    if pickedPlayer then
+        PLAYER.Character.HumanoidRootPart.CFrame = pickedPlayer.Character.HumanoidRootPart.CFrame
+    end
+end)
+
+local Tab4 = Init:NewTab("Mix")
+local Section5 = Tab4:NewSection("Granades")
+local Button2 = Tab4:NewButton("Anti-Flashbang", function()
+	PLAYER.PlayerGui.Blnd:Destroy()
+end)
+local Button2 = Tab4:NewButton("Anti-Smoke", function()
+	worksp = PLAYER.Character.Parent
+	print("Got Workspace")
+	worksp["Ray_Ignore"].Smokes:Destroy()
+end)
+
+
+for i,plr in pairs(game.Players:GetChildren()) do
+    AddBox(plr)
+	AddTracers(plr)
+end
+
+game.Players.PlayerAdded:Connect(function(plr)
+    AddBox(plr)
+	AddTracers(plr)
+end)
+
+game:GetService('RunService').RenderStepped:connect(function()
+
+	-- Aimbot Check
+	if AIMBOTXD then 
+		local _pos = CameraGetClosestToMouse(70000)
+		if _pos then
+			aimAt(_pos, 0)
+		end
+	end 
+
+	-- Fov
+	local acc = 0 / 2	
+	local posd = UIS:GetMouseLocation() 
+	FOVFFrame.Position = UDim2.new(0, posd.X, 0, posd.Y - 36)
+	FOVFFrame.Size = UDim2.new(0, 70000 + acc, 0, 70000 + acc)
+	FOVFFrame.Visible = false
+	FOVFFrame.Transparency = 1
+end)
